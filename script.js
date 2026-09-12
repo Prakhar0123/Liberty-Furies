7fc0a3567a39e42988d785db2ddbcfe774720824
Prakhar Kushwaha <136454803+Prakhar0123@users.noreply.github.com>
2026-09-12 22:42:47 +0530

    script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function renderJobs(jobs) {
       </div>`;
     return;
   }
-const isHome = window.location.pathname.includes('index.html') || window.location.pathname === '/' || window.location.pathname.endsWith('/KARMA/');
+  const isHome = window.location.pathname.includes('home.html') || window.location.pathname === '/' || window.location.pathname.endsWith('/');
   jobs.forEach(job => {
     const card = document.createElement('div');
     card.className = 'job-card';
